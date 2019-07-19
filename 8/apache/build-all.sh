65d6dbb3aac451d323f7eab0cf9447e463c59bd1
Alan Dixon <alan.g.dixon@gmail.com>
2019-07-19 10:19:17 -0400

    Updated 8/apache to support local builds

---
+++ 8/apache/build-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# a convenience script to build all the prosimulium containers locally
+# by overriding the default ARG REPOSITORY_FROM=blackflysolutions/
+docker build drupal/ -t prosimulium:8-apache
+docker build --build-arg REPOSITORY_FROM='' admin/ -t prosimulium:8-apache-admin
+docker build --build-arg REPOSITORY_FROM='' editor/ -t prosimulium:8-apache-editor
