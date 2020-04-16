ebb67425b8772cafed0605ca1adcc9ddd127b731
Alan Dixon <alan.g.dixon@gmail.com>
2020-04-16 10:40:17 -0400

    Add extern/.htaccess file after upgrade, include script to get l10n files

--- 8/apache/admin/civicrm-upgrade.sh
+++ 8/apache/admin/civicrm-upgrade.sh
@@ -2,6 +2,7 @@
 # Use my composer to update my civicrm version
 echo "Updating CiviCRM Codebase using composer."
 cd /var/www/drupal
-sudo -u drupal composer update civicrm/civicrm-core civicrm/civicrm-drupal-8 roundearth/civicrm-composer-plugin
+sudo -u drupal composer update civicrm/civicrm-core civicrm/civicrm-drupal-8 roundearth/civicrm-composer-plugin --with-dependencies
 cv upgrade:db
 chown -R www-data:www-data web/sites/default/files/civicrm
+echo 'RewriteEngine Off' > /var/www/drupal/web/libraries/civicrm/extern/.htaccess
