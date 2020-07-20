e8a40e397a8f257ed3314c926762e3b170229a38
Alan Dixon <alan.g.dixon@gmail.com>
2020-07-20 11:07:31 -0400

    convert from roundearth to buildkit method of civicrm d8 install

--- 8/apache/admin/civicrm-code-install.sh
+++ 8/apache/admin/civicrm-code-install.sh
@@ -6,8 +6,9 @@ cd /var/www/drupal
 if [ -d "/var/www/drupal/vendor/civicrm" ]; then
   echo "Detected existing CiviCRM codebase, aborted."
 else
-  sudo -u drupal composer require --no-update --dev roundearth/civicrm-composer-plugin
+  sudo -u drupal composer require --no-update civicrm/civicrm-asset-plugin:'~1.0.0' pear/pear_exception:'1.0.1 as 1.0.0' --prefer-source
   sudo -u drupal composer require --no-update civicrm/civicrm-core:${VSITE_CIVICRM_VER}
+  sudo -u drupal composer require --no-update civicrm/civicrm-packages:${VSITE_CIVICRM_VER}
   sudo -u drupal composer require --no-update civicrm/civicrm-drupal-8:${VSITE_CIVICRM_VER}
   sudo -u drupal composer update
   echo "CiviCRM codebase installed."
