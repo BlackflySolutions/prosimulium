a553cd9fe4a0d35db378b23a374d6b50815202cf
Alan Dixon <alan.g.dixon@gmail.com>
2020-09-17 22:35:02 -0400

    include drupal version in compose create-project

--- 8/apache/admin/admin.sh
+++ 8/apache/admin/admin.sh
@@ -5,7 +5,7 @@ if [ -z "$(ls -A /var/www/drupal)" ]; then
   echo "Empty codebase, seting up latest stable Drupal code using composer."
   cd /var/www/drupal
   chown drupal .
-  sudo -u drupal composer create-project drupal/recommended-project . --no-interaction  --no-install
+  sudo -u drupal composer create-project drupal/recommended-project:${VSITE_DRUPAL_VER} . --no-interaction  --no-install
   sudo -u drupal composer require --no-update drush/drush zaporylie/composer-drupal-optimizations:^1.1 --dev
   sudo -u drupal composer update
   chmod g+w web/sites/default/
