ce0c83b1e45f96afd5fbf4bedbe803bbd7baffa0
Alan Dixon <alan.g.dixon@gmail.com>
2023-01-25 13:53:46 -0500

    enable customization of apache config + include aliases for code that does not know about multilingual path prefixes

--- 9/apache/drupal/initialize.sh
+++ 9/apache/drupal/initialize.sh
@@ -2,3 +2,12 @@
 # Set the mail, varnish and redis host domains to the docker host ip for this container network.
 VHOST_DOCKER_IP=`/sbin/ip route | awk '/default/ { print $3 }'`
 echo "$VHOST_DOCKER_IP mail varnish $VSITE_DOMAIN" >> /etc/hosts
+# support for customizing your apache config, not recommended
+if [ -f "/var/www/drupal/web/sites/default/conf/apache-custom.conf" ]; then
+  cp /var/www/drupal/web/sites/default/conf/apache-custom.conf /etc/apache2/conf-available/vsite-custom.conf
+  a2enconf vsite-custom
+fi 
+# support for a custom robots.txt
+if [ -f "/var/www/drupal/web/sites/default/conf/robots.txt" ]; then
+  cp /var/www/drupal/web/sites/default/conf/robots.txt /var/www/drupal/web/
+fi 
